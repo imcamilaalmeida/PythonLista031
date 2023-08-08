ebe2c4c8cb31437a32b7f6a01604781fde79b440
Camila Almeida <camilaalma2008@gmail.com>
2023-08-08 09:08:59 -0300

    novos operadores

--- operadores/operadores.py
+++ operadores/operadores.py
@@ -1,6 +1,8 @@
 a = 3
 b = -4
 
+#Operadores aritmetricos
+
 soma = a + b
 sub = a - b
 mult = a * b
@@ -14,3 +16,28 @@ print(f"Divisão: {div}")
 print(f"Divisão de inteiros: {div_inteiros}")
 print(f"Cálculos dentro do print: {a * b}")
 print(f"Resto da divisão de 16 por a: {16 % a}")
+
+#operadores relacionais
+
+print(f"a == b: {a == b}")
+print(f"a < 5: {a < 5}")
+print(f"a > b: {a > b}")
+print(f"a <= 3: {a <= 3}")
+print(f"a >= 4: {a >= 4}")
+print(f"a != b: {a != b}")
+
+c = a != b
+print(f"c: {c}")
+print(f"Tipo da var c: ", type(c)) #bool é booleano ou logico
+
+
+#operadores logicos
+logic1 = True
+logic2 = False
+print(f"type(logic1): {type(logic1)}")
+print(f"type(logic2): {type(logic2)}")
+
+print(f"logic1: {logic1}")
+print(f"not logic1: {not logic1}")
+print(f"logic1 or logic2: {logic1 or logic2}")
+print(f"logic1 and logic2: {logic1 and logic2}")
